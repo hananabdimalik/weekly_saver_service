46b573573313af6d85bd0a399cc511978ea9291d
Hanan Abdimalik <hanan.abdimalik@Hanans-MacBook-Pro.local>
2026-05-18 14:28:14 +0100

    first commit

---
+++ src/main/kotlin/com/space/weekly_saver_service/WeeklySaverServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.space.weekly_saver_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WeeklySaverServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<WeeklySaverServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/space/weekly_saver_service/WeeklySaverServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.space.weekly_saver_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WeeklySaverServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
